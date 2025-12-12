1ec54e727082190dfaa5936c8576fb7cdeca001d
somaya <somaiabaghdady@gmail.com>
2025-12-12 14:16:53 +0200

    this is the math function originally in math branch

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include<iostream>
 using namespae std;
+int sum(int a, int b) {
+	return a + b;
+}
 void main() {
 	cout << "hello world!";
-
+	cout << "the summation of a,b =" << sum(50, 100) << endl;
 }
 
